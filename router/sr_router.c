5f8d77ffd9e177e342935446a48c1b1f284e4438
tuu18yu <82858704+tuu18yu@users.noreply.github.com>
2022-10-19 12:56:52 -0400

    Added handle_arp for case when ARP is requesting

--- router/sr_router.c
+++ router/sr_router.c
@@ -66,15 +66,78 @@ void sr_handlepacket(struct sr_instance* sr,
   printf("*** -> Received packet of length %d \n",len);
 
   /* fill in code here */
+  if (len < sizeof(sr_ethernet_hdr_t)) {
+    fprintf(stderr,"Error: Packet is too small\n");
+    return;
+  }
 
   if (ethertype(packet) == ethertype_ip){
     printf("Received packet is IP\n");
   }
-
-  else if(ethertype(packet)== ethertype_arp){
+  else if (ethertype(packet) == ethertype_arp){
     printf("Received packet is ARP\n");
+    handle_arp(sr, packet, len, interface);
   }
-
 
 }/* end sr_ForwardPacket */
 
+void handle_arp(struct sr_instance* sr,
+        uint8_t * packet/* lent */,
+        unsigned int len,
+        char* interface/* lent */)
+{
+  assert(sr);
+  assert(packet);
+  assert(interface);
+
+  if (len < sizeof(sr_ethernet_hdr_t) + sizeof(sr_arp_hdr_t)) {
+    fprintf(stderr,"Error: Packet is too small\n");
+    return;
+  }
+  sr_arp_hdr_t *received_arp_hdr =  (sr_arp_hdr_t *) (packet + sizeof(sr_ethernet_hdr_t));
+  sr_ethernet_hdr_t *received_ether_hdr =  (sr_ip_hdr_t *) (packet + sizeof(sr_ethernet_hdr_t));
+  struct sr_if *current_interface = sr_get_interface(sr, interface);
+
+  /* ARP packet is requesting */
+  if (arp_op_request == ntohs(received_arp_hdr->ar_op)) {
+    if (received_arp_hdr->ar_tip != current_interface->ip) {
+      printf("ARP packet is not targeting this interface\n");
+      return;
+    }
+    printf("ARP packet is requesting")
+    // Send reply packet to the sender
+    uint8_t *reply_packet = (uint8_t *) malloc(len);
+
+    /* Set values for reply ethernet header */
+    sr_ethernet_hdr_t *reply_ether_hdr =  (sr_ip_hdr_t *) (reply_packet + sizeof(sr_ethernet_hdr_t));
+    // the source address is the address of router's current interface
+    memcpy(reply_ether_hdr->ether_shost, current_interface->addr, sizeof(uint8_t)*ETHER_ADDR_LEN);
+    // the destination address is the source address of received packet
+    memcpy(reply_ether_hdr->ether_shost, received_arp_hdr->ether_shost, sizeof(uint8_t)*ETHER_ADDR_LEN);
+    reply_ether_hdr->ether_type = htons(ethertype_arp);
+
+    /* Set values for reply ARP header */
+    sr_arp_hdr_t *reply_arp_hdr =  (sr_arp_hdr_t *) (reply_packet + sizeof(sr_ethernet_hdr_t));
+    reply_arp_hdr->ar_hrd = received_arp_hdr->ar_hrd;
+    reply_arp_hdr->ar_pro = received_arp_hdr->ar_pro;
+    reply_arp_hdr->ar_hln = received_arp_hdr->ar_hln;
+    reply_arp_hdr->ar_pln = received_arp_hdr->ar_pln;
+    reply_arp_hdr->ar_op = htons(arp_op_reply);
+    // Sender of the reply packet is the router's current interface
+    memcpy(reply_arp_hdr->ar_sha, current_interface->addr, ETHER_ADDR_LEN);
+    reply_arp_hdr->ar_sip = current_interface->ip;
+    // Target of the reply packet is the source of received packet
+    memcpy(reply_arp_hdr->ar_tha, received_arp_hdr->ether_shost, ETHER_ADDR_LEN);
+    reply_arp_header->ar_tip = received_arp_hdr->ar_sip;
+
+    // Send the reply packet and free the malloc space
+    sr_send_packet(sr, reply_packet, len, interface);
+    free(reply_packet);
+  }
+
+  /* ARP packet is replying */
+  else if (arp_op_reply == ntohs(received_arp_header->ar_op)) { 
+    printf("ARP packet is replying")
+  }
+    return;
+}

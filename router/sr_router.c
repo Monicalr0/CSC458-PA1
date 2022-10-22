0d182ebc89b0fb1f058e6584525717fc3f5f4172
tuu18yu <82858704+tuu18yu@users.noreply.github.com>
2022-10-22 15:21:44 -0400

    Update sr_router.c

--- router/sr_router.c
+++ router/sr_router.c
@@ -307,7 +307,9 @@ void handle_ip(struct sr_instance *sr,
     ip_hdr->ip_sum = cksum(ip_hdr, sizeof(sr_ip_hdr_t));
 
     /* Check routing table, perform Longest Prefix Match */
+    printf("Look prefix in routing table - handle ip");
     struct sr_rt *longest_prefix = longest_prefix_match(sr, ip_hdr->ip_dst);
+    printf("Returned from longest_prefix_match - handle ip");
 
     /*If no matching found, drop packet and send unreachable*/
     if (!longest_prefix)
